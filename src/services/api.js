1c8450efd8179f3ee1099567df2007edcb3a0bc1
CristianV22 <cristian.v.s.oliveira@gmail.com>
2025-06-26 23:00:57 -0300

    Connection with backend in prod

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
 export const api = axios.create({
+  baseURL: 'https://todolist-1u1y.onrender.com', // production
   baseURL: 'http://localhost:3000', // back-end local
   // baseURL: 'http://backend:3000', // back-end local
 });
