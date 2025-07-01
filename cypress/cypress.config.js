9e72ec63816d23891311928b1f5d80444ece1d22
CristianV22 <cristian.v.s.oliveira@gmail.com>
2025-06-30 22:04:13 -0300

    Fixing test flakiness

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // baseUrl: "http://frontend:5173", // nome do serviço no Docker
+    // baseUrl: "http://frontend:5173",
     // baseUrl: "http://localhost:5173/",
     baseUrl: "http://localhost:3000/",
     supportFile: false,
