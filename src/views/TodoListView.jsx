1620f7fc80c4719efe84173c00d3bc8e3b87d8c1
CristianV22 <cristian.v.s.oliveira@gmail.com>
2025-07-10 18:40:11 -0300

    Testing for presentation

--- src/views/TodoListView.jsx
+++ src/views/TodoListView.jsx
@@ -7,7 +7,7 @@ export function TodoListView() {
 
   return (
     <main>
-      <h1>Todolist updated!</h1>
+      <h1>Todolist</h1>
       <TodoInput onAdd={addTodo} />
       <ul>
         {todos.map((todo) => (
